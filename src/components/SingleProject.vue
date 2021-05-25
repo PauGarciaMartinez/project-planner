70fa56d111fe720e406fb76ced67a15f68eff2d8
Pau Garcia Martínez <etpau@hotmail.es>
2021-05-25 20:19:34 +0200

    Display project info and style component

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,14 +1,31 @@
 <template>
-  <p>{{ project.title }}</p>
-        <p>{{ project.details }}</p>
+  <div class="project">
+    <div class="actions">
+      <h3>{{ project.title }}</h3>
+    </div>
+    <div class="details">
+      <p>{{ project.details }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'SingleProject'
+  name: 'SingleProject',
+  props: ['project']
 }
 </script>
 
-<style>
-
+<style scoped>
+.project {
+  margin: 20px auto;
+  background: white;
+  padding: 10px 20px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 3px rgba(0,0,0,0.05);
+  border-left: 4px solid #e90074;
+}
+h3 {
+  cursor: pointer;
+}
 </style>
