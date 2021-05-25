5ee02c4cbe17eaca7038f984392fe72d15cdda68
Pau Garcia Martínez <etpau@hotmail.es>
2021-05-25 20:06:11 +0200

    Fetch and store projects data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,17 @@ export default {
   name: 'Home',
   components: {
     
+  },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
   }
 }
 </script>
