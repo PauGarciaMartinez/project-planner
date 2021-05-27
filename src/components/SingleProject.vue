36f941a6c78c994283361642927a98fa78831e1b
Pau Garcia Martínez <etpau@hotmail.es>
2021-05-27 23:55:57 +0200

    Style completed projects

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="project">
+  <div class="project" :class="{ complete: project.complete }">
     <div class="actions">
       <h3 @click="toggleVisibility">{{ project.title }}</h3>
       <div class="icons">
         <span class="material-icons">edit</span>
         <span @click="deleteProject" class="material-icons">delete</span>
-        <span @click="toggleComplete" class="material-icons">done</span>
+        <span @click="toggleComplete" class="material-icons tick">done</span>
       </div>
     </div>
     <div class="details" v-show="visible">
@@ -71,5 +71,11 @@ h3 {
 }
 .material-icons:hover {
   color: #777;
+}
+.project.complete {
+  border-left-color: #00ce89;
+}
+.project.complete .tick {
+  color: #00ce89;
 }
 </style>
