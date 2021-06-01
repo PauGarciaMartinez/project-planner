6c0b77ac9e9683a98943c426d6c31ebca1b796cf
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-01 14:56:28 +0200

    Create a filter nav component and set filter methods

---
+++ src/components/FilterNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="filter-nav">
+    <button 
+      @click="updateFilter('all')"
+    >View all</button>
+    <button 
+      @click="updateFilter('completed')"
+    >Completed</button>
+    <button 
+      @click="updateFilter('ongoing')"
+    >Ongoing</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  methods: {
+    updateFilter(by) {
+      this.$emit('filterChange', by)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
